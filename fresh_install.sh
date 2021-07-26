80b94a8750b29e54c53a9a48c8db098e95c16789
Anders Langlands <anderslanglands@gmail.com>
2021-07-26 17:03:24 +1200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -35,10 +35,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Dev
 
 ## cmake
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update
 sudo apt install cmake
+
+sudo apt install ninja-build
 
 # Rust
 
